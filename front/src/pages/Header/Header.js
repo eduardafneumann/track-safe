4c6019915c9359943cfcfccadba4e0e5161101f7
Eduarda Neumann <eduardafneumann@gmail.com>
2024-05-24 21:02:04 -0300

    adicionar link para home no header

--- front/src/pages/Header/Header.js
+++ front/src/pages/Header/Header.js
@@ -1,12 +1,15 @@
 import './Header.css'
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './../../images/logo.jpg';
 
 function Header() {
   return (
     <header>
     <div className='header'>
-      <img src={logo} alt="Logo"></img>
+      <Link to="/">
+        <img className='logo' src={logo} alt="Logo"></img>
+      </Link>
     </div>
     </header>
   );
